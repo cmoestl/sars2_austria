c04fece5f5304045c6784489eef1e9263ae02ce5
cmoestl <christian.moestl@oeaw.ac.at>
2020-03-23 08:10:18 +0100

    daily

--- set_input_here.py
+++ set_input_here.py
@@ -4,12 +4,12 @@ filename='austria_south_korea.png'
 
 #daily number of total confirmed cases
 cases_list=[2, 2, 3, 3, 9, 14, 18, 21, 29, 41, 55, 79, 104, 131, 182, \
-                246, 302, 504, 655, 860, 1016,1332, 1646,2053,2388,2814]
+                246, 302, 504, 655, 860, 1016,1332, 1646,2053,2388,2814,3244]
 
 #Start time for the data in cases_list (23:00 because it should contain the final data for the day)
 t_start_string='2020-02-25 23:00'
 #end time for the data in cases_list  day
-t_end_string='2020-03-21 23:00'
+t_end_string='2020-03-22 23:00'
 
 
 #shift in days for south korea
